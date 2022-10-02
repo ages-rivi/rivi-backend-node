36593e55068100a13c09119ec176589cd60e1888
Fernando <fernando.elger@edu.pucrs.br>
2022-10-02 05:03:08 -0300

    feat: update pesquisadores routes based on pesquisador controller

--- routes/pesquisadores.js
+++ routes/pesquisadores.js
@@ -1,68 +1,36 @@
-const { PrismaClient } = require('@prisma/client')
-const express = require('express')
-const prisma = new PrismaClient()
+const express = require('express');
+const {
+  createPesquisador,
+  getPesquisadores,
+  getPesquisador,
+  updatePesquisador,
+  deletePesquisador,
+} = require('../controllers/pesquisadorController');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({mssg: 'RETORNA todos os pesquisadores'})
-})
+router.get('/', (getPesquisadores) => {
+  res.json({ mssg: 'RETORNA todos os pesquisadores' });
+});
 
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'RETORNA um pesquisador'})
-})
+router.get('/:id', (getPesquisador) => {
+  res.json({ mssg: 'RETORNA um pesquisador' });
+});
 
 router.post('/', (req, res) => {
-    res.json({mssg: 'CRIA um novo pesquisador'})
-})
+  res.json({ mssg: 'CRIA um novo pesquisador' });
+});
 
-router.post('/adicionaPesquisador', async (req, res) => {
-    const { nome, email, descricao, afiliacao, tag, foto, contatos, projetos, artigos} = req.body
-    try {
-        const pesquisador = await prisma.pesquisador.create({
-            data: {
-                nome,
-                email,
-                descricao,
-                afiliacao,
-                tag: tag,
-                foto: foto,
-                contatos: contatos,
-                projetos: {
-                    create: [
-                        projetos.titulo,
-                        projetos.descricao,
-                        projetos.estado,
-                        projetos.tag,
-                    ]
-                },
-                artigos: {
-                    create: [
-                        artigos.doi,
-                        artigos.titulo,
-                        artigos.descricao,
-                        artigos.citacao,
-                        artigos.afiliacao,
-                        artigos.link,
-                        artigos.tag,
-                    ]
-                }
-            }
-        })
-        res.status(200).json(pesquisador)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }   
+router.post('/adicionaPesquisador', (createPesquisador) => {
+  res.json({ mssg: 'CRIA um novo pesquisador' });
+});
 
-    res.json({mssg: 'CRIA um novo pesquisador'})
-})
+router.patch('/:id', (updatePesquisador) => {
+  res.json({ mssg: 'ATUALIZA um pesquisador' });
+});
 
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETA um pesquisador'})
-})
+router.delete('/:id', (deletePesquisador) => {
+  res.json({ mssg: 'DELETA um pesquisador' });
+});
 
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'ATUALIZA um pesquisador'})
-})
-
-module.exports = router
+module.exports = router;
