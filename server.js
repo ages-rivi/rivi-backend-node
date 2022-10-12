c61db2be3e8e23bcd18c94edf61067aacbdd3bb5
Fernando Elger <fernando.elger@acad.pucrs.br>
2022-10-11 22:27:18 -0300

    feat: adicionado documentacao das rotas de projeto no swagger

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 // express app
 const app = express();
 app.use(express.json());
-// middleware
+
+// register swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+console.log('Swagger running on http://localhost:4000/api-docs');
+
+// register middleware
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
@@ -17,13 +24,14 @@ mongoose
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to mongo & listening on port', process.env.PORT);
+      console.log('Connected to mongo & listening on port', process.env.PORT);
     });
   })
   .catch((error) => {
     console.log(error);
   });
 
+// set routes
 app.use('/api/pesquisadores', require('./routes/pesquisadores'));
 app.use('/api/artigos', require('./routes/artigos'));
 app.use('/api/projetos', require('./routes/projetos'));
