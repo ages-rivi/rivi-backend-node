2f8d15af047084f947d6049e6d340db40cff4df8
Fernando Elger <fernando.elger@acad.pucrs.br>
2022-10-29 19:27:15 -0300

    refac: alteração da estrutura de pastas do projeto

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ const swaggerOptions = {
       },
     ],
   },
-  apis: ['./routes/*.js'],
+  apis: ['./src/routes/*.js'],
 };
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
