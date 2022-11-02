1f8767dc8a89d328b394670c3a45c6cebf6656fc
Fernando Elger <fernando.elger@acad.pucrs.br>
2022-11-02 18:05:40 -0300

    feat: adicionado camada de serviço e corrigo controller para isso

--- src/controllers/projetoController.js
+++ src/controllers/projetoController.js
@@ -1,25 +1,9 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+var service = require('../services/projetoService');
 
 // GET - retorna todos projetos
 const getAllProjetos = async (req, res) => {
   try {
-    const projetos = await prisma.projeto.findMany({
-      select: {
-        id: true,
-        titulo: true,
-        descricao: true,
-        estado: true,
-        tags: true,
-        pesquisadores: {
-          select: {
-            nome: true,
-            afiliacao: true,
-          },
-        },
-      },
-    });
-    console.log(projetos);
+    const projetos = await service.getAllProjetos(req, res);
     return res.status(200).json(projetos);
   } catch (error) {
     return res.status(400).json({ error: error.message });
@@ -29,29 +13,7 @@ const getAllProjetos = async (req, res) => {
 // GET - retorna um único projeto
 const getProjeto = async (req, res) => {
   try {
-    const { id } = req.params;
-    const projeto = await prisma.projeto.findUnique({
-      where: {
-        id: id,
-      },
-      select: {
-        id: true,
-        titulo: true,
-        descricao: true,
-        estado: true,
-        tags: true,
-        pesquisadores: {
-          select: {
-            nome: true,
-            afiliacao: true,
-          },
-        },
-      },
-    });
-    if (!projeto) {
-      return res.status(404).json({ error: 'Não foi possível encontrar este projeto.' });
-    }
-    console.log(projeto);
+    const projeto = await service.getProjeto(req, res);
     return res.status(200).json(projeto);
   } catch (error) {
     return res.status(400).json({ error: error.message });
@@ -60,24 +22,8 @@ const getProjeto = async (req, res) => {
 
 // POST - cria um projeto
 const createProjeto = async (req, res) => {
-  const { titulo, descricao, estado, tags, pesquisadoresIds } = req.body;
-
   try {
-    let novosPesquisadores = pesquisadoresIds.map((element) => {
-      return { id: element };
-    });
-    projeto = await prisma.projeto.create({
-      data: {
-        titulo,
-        descricao,
-        estado,
-        tags: tags != null ? tags : undefined,
-        pesquisadores: {
-          connect: novosPesquisadores,
-        },
-      },
-    });
-    console.log(projeto);
+    const projeto = await service.createProjeto(req, res);
     return res.status(201).json(projeto);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -87,60 +33,7 @@ const createProjeto = async (req, res) => {
 // UPDATE - atualiza um projeto
 const updateProjeto = async (req, res) => {
   try {
-    const { id } = req.params;
-    const { titulo, descricao, estado, tags, pesquisadores } = req.body;
-
-    let projeto = await prisma.projeto.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    if (!projeto) {
-      return res.status(404).json({ error: 'Não foi possível encontrar este projeto.' });
-    }
-
-    if (pesquisadores != null) {
-      let deletaPesquisadores = projeto.pesquisadoresIds
-        .filter((element) => pesquisadores.includes(element))
-        .map((element) => {
-          return { id: element };
-        });
-      let adicionaPesquisadores = pesquisadores
-        .filter((element) => !projeto.pesquisadoresIds.includes(element))
-        .map((element) => {
-          return { id: element };
-        });
-
-      if (deletaPesquisadores.length > 0) {
-        projeto = await prisma.projeto.update({
-          where: { id: id },
-          data: {
-            pesquisadores: { disconnect: deletaPesquisadores },
-          },
-        });
-      }
-      if (adicionaPesquisadores.length > 0) {
-        projeto = await prisma.projeto.update({
-          where: { id: id },
-          data: {
-            pesquisadores: { connect: adicionaPesquisadores },
-          },
-        });
-      }
-    }
-
-    projeto = await prisma.projeto.update({
-      where: {
-        id: id,
-      },
-      data: {
-        titulo,
-        descricao,
-        estado,
-        tags: tags,
-      },
-    });
-    console.log(projeto);
+    const projeto = await service.updateProjeto(req, res);
     return res.status(200).json(projeto);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -150,22 +43,7 @@ const updateProjeto = async (req, res) => {
 // DELETE - deleta um projeto
 const deleteProjeto = async (req, res) => {
   try {
-    const { id } = req.params;
-    let projeto = await prisma.projeto.findUnique({
-      where: {
-        id: id,
-      },
-    });
-    if (!projeto) {
-      return res.status(404).json({ error: 'Não foi possível encontrar este projeto.' });
-    }
-
-    await prisma.projeto.delete({
-      where: {
-        id: id,
-      },
-    });
-    console.log(projeto);
+    await service.deleteProjeto(req, res);
     return res.status(200).json({ mssg: 'Projeto deletado!' });
   } catch (error) {
     res.status(400).json({ error: error.message });
